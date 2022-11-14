5f9f5b230fe41673ed71d771bdee88fc09808feb
Sebastián Quiroz <66489459+Seba123XDHD2@users.noreply.github.com>
2022-11-14 18:06:16 -0300

    nivel 1 avance

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public int rowa = 3;
-    public int columns = 3;
+    public int rowa = 10;
+    public int columns = 10;
     public GameObject GridPrefab;
     public Vector3 LeftButtom = new Vector3(0, 0, 0);
     public float scale;
     public GameObject[,] gridArray;
+    public List<GameObject> players, enemies;
+    public List<Vector2Int> playerPos, enemiesPos;
+    
 
 
     
@@ -17,6 +20,20 @@ public class GridManager : MonoBehaviour
     void Start()
     {
         GeneratorGrid();
+        playerPos = new List<Vector2Int>();
+        enemiesPos = new List<Vector2Int>();
+        for (int i = 0; i < players.Count; i++)
+        {
+            playerPos.Add(Vector2Int.zero);
+        }
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemiesPos.Add(Vector2Int.zero);
+        }
+
+        playerPos[0] = Place(players[0], 0,0, false, Vector2Int.zero);
+        enemiesPos[0] = Place(enemies[0],0,1, false, Vector2Int.zero);
+
     }
 
     // Update is called once per frame
@@ -26,21 +43,31 @@ public class GridManager : MonoBehaviour
     }
     void GeneratorGrid()
     {
+        gridArray = new GameObject[columns, rowa];
         for (int i = 0; i < columns; i++)
         {
             for (int j = 0; j < rowa; j++)
             {
                 GameObject obj = Instantiate(GridPrefab);
                 obj.transform.position = new Vector3(0 + scale*i, 0+scale*j, 0);
-
+                GridStat gs = obj.GetComponent<GridStat>();
+                gs.x = i;
+                gs.y = j;
                 obj.transform.SetParent(gameObject.transform);
-                
-               
-
-
-                
-
+                gridArray[i, j] = obj;
             }
         }
+    }
+
+    Vector2Int Place(GameObject pawn, int x, int y, bool lastP, Vector2Int lPos)
+    {
+        if (lastP)
+        {
+            gridArray[lPos.x, lPos.y].GetComponent<GridStat>().visited = -1;
+        }
+        pawn.transform.position = new Vector3(gridArray[x, y].transform.position.x, gridArray[x, y].transform.position.y, 1);
+        gridArray[x, y].GetComponent<GridStat>().visited = 1;
+
+        return new Vector2Int(x,y);
     }
 }
