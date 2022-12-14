016407a493a54b880994e4fe27f0657b3db09fe7
Seba123XDHD <sebiquirozbad@gmail.com>
2022-12-14 20:21:15 -0300

    invoke

--- Assets/cata/InvokeRepe.cs
+++ Assets/cata/InvokeRepe.cs
@@ -33,7 +33,7 @@ public class InvokeRepe : MonoBehaviour
         ObjectsList[Random.Range(0, ObjectsList.Length)].SetActive(true);
         ObjectsList[Random.Range(0, ObjectsList.Length)].SetActive(true);
         ObjectsList[Random.Range(0, ObjectsList.Length)].SetActive(true);
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(10);
 
     }
 }
