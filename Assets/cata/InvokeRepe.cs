a87519aef95dab7990fc9c7f50633d20e100ad0a
Seba123XDHD <sebiquirozbad@gmail.com>
2022-12-21 23:19:15 -0300

    mejorando el ataque del castillo

--- Assets/cata/InvokeRepe.cs
+++ Assets/cata/InvokeRepe.cs
@@ -5,34 +5,44 @@ using UnityEngine;
 public class InvokeRepe : MonoBehaviour
 {
     public GameObject[] ObjectsList; // Not only 18, this script will except any number of game objects you put here.
+    public GameObject Jefe;
+    
+    public int ataqueBoss;
+    public int Hpbossactual;
+    public int HpBossTotal;
 
     void Start()
     {
         StartCoroutine(ResetInvoKe());
-
+       
+        
     }
     private void Update()
     {
-        
+        Hpbossactual = Jefe.GetComponent<datosEnemigos>().vidaEnemigo;
     }
     IEnumerator ResetInvoKe()
     {
         List<int> posibles = new List<int>();
+      
         for (int i = 0; i < ObjectsList.Length; i++)
         {
             ObjectsList[i].SetActive(false);
             posibles.Add(i);
         }
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 4+Mathf.Clamp((HpBossTotal /Hpbossactual) * 3, 0, 3) ; i++)
         {
             int selected = Random.Range(0, posibles.Count);
             ObjectsList[posibles[selected]].SetActive(true);
             posibles.Remove(selected);
-        }
+            Hpbossactual = Jefe.GetComponent<datosEnemigos>().vidaEnemigo;
 
+            Debug.Log(Hpbossactual);
+
+        }
         yield return new WaitForSeconds(10);
         ResetInvoKe();
-
+       
     }
 }
--- Assets/cata/ataque/datosEnemigos.cs
+++ Assets/cata/ataque/datosEnemigos.cs
@@ -15,5 +15,6 @@ public class datosEnemigos : MonoBehaviour
             imagen.SetActive(false);
 
         }
+        
     }
 }
