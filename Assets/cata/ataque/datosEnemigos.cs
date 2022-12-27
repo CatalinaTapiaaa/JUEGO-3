476b79039d537f5314851932ecfd2a85d900db83
Seba123XDHD <sebiquirozbad@gmail.com>
2022-12-27 07:32:06 -0300

    Revert "aaa"
    This reverts commit 20d2b7e521e2bcf619f24866df2eb3e0ba47751c.

--- Assets/cata/ataque/datosEnemigos.cs
+++ Assets/cata/ataque/datosEnemigos.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class datosEnemigos : MonoBehaviour
 {
     public int vidaEnemigo;
+    public Slider vidaVisual;
+    public Text text;
     public GameObject imagen;
 
     private void Update()
     {
-        if(vidaEnemigo <= 0)
+        vidaVisual.GetComponent<Slider>().value = vidaEnemigo;
+        text.text = "" + vidaEnemigo;                  
+
+
+        if (vidaEnemigo <= 0)
         {
             Destroy(gameObject);
             imagen.SetActive(false);
-
+            
         }
         
     }
