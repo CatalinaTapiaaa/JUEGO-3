196ce02d3c5250ab6846eb6adf4799c89a12fb38
Seba123XDHD <sebiquirozbad@gmail.com>
2022-12-27 07:25:29 -0300

    aaa

--- Assets/cata/ataque/dañoPlayer.cs
+++ Assets/cata/ataque/dañoPlayer.cs
@@ -6,16 +6,12 @@ public class dañoPlayer : MonoBehaviour
 {
     public int damage;
     public GameObject Player;
-    public GameObject objeto;
-
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag =="Player")
         {
            Player.GetComponent<DatosJugador>().vidaPlayer -=damage;
-           objeto.gameObject.SetActive(true);
-
         }
     }
 }
